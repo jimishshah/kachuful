2fcefbb464e313187a486d97281a092acf9acb28
Jimish Shah - Content Discovery <jimish.shah02.ext@bbc.co.uk>
2020-04-26 15:07:42 +0100

    Fix bug, Make new sequence if one of the players are disconnected from the game

---
+++ server-tests/lambdas/websockets/disconnect.test.js
@@ -0,0 +1,79 @@
+process.env = { tableName: "sample-table" };
+const Dynamo = require("../../../server/lambdas/common/dynamo");
+const WebSocket = require("../../../server/lambdas/common/web-socket-message");
+const disconnectHandler = require("../../../server/lambdas/websockets/disconnect")
+  .handler;
+
+jest.mock("../../../server/lambdas/common/dynamo", () => {
+  function createPlayers(count) {
+    return Array.from(Array(count).keys()).map((index) => ({
+      ID: `ID${index + 1}`,
+      domainName: "test-domainName" + index,
+      stage: "int-test",
+      playerName: "anna" + index,
+      lastLevel: "7",
+      lastTrumpColour: "heart",
+      sequenceNumber: index + 1,
+    }));
+  }
+  return {
+    get: jest.fn().mockReturnValue({
+      ID: "ID1",
+      domainName: "test-domainName1",
+      stage: "int-test",
+      playerName: "anna1",
+      lastLevel: "7",
+      lastTrumpColour: "heart",
+      sequenceNumber: 1,
+    }),
+    write: jest.fn(),
+    delete: jest.fn(),
+    scan: jest.fn().mockReturnValue({
+      Items: [
+        {
+          ID: `ID2`,
+          domainName: "test-domainName",
+          stage: "int-test",
+          playerName: "anna",
+          lastLevel: "7",
+          lastTrumpColour: "heart",
+          sequenceNumber: 2,
+        },
+        {
+          ID: `ID3`,
+          domainName: "test-domainName",
+          stage: "int-test",
+          playerName: "anna",
+          lastLevel: "7",
+          lastTrumpColour: "heart",
+          sequenceNumber: 3,
+        },
+      ],
+    }),
+  };
+});
+jest.mock("../../../server/lambdas/common/web-socket-message", () => ({
+  send: jest.fn().mockReturnValue(Promise.resolve("something")),
+}));
+
+test("when a player gets disconnected, that player is deleted and new sequence is given to everyone", async () => {
+  const event = {
+    body: JSON.stringify({
+      message: "",
+    }),
+    requestContext: {
+      connectionId: 0,
+    },
+  };
+  await disconnectHandler(event);
+
+  expect(Dynamo.delete).toHaveBeenCalledWith(0, "sample-table");
+
+  const player1 = Dynamo.write.mock.calls[0][0];
+  const player2 = Dynamo.write.mock.calls[1][0];
+
+  expect(player1.sequenceNumber).toEqual(1);
+  expect(player1.ID).toEqual(`ID2`);
+  expect(player2.sequenceNumber).toEqual(2);
+  expect(player2.ID).toEqual(`ID3`);
+});
--- server/lambdas/websockets/disconnect.js
+++ server/lambdas/websockets/disconnect.js
@@ -9,9 +9,20 @@ exports.handler = async (event) => {
     const { connectionId: connectionID } = event.requestContext;
     const { tableId } = await Dynamo.get(connectionID, tableName);
     await Dynamo.delete(connectionID, tableName);
+    await updateSequence(tableId);
     await updatePlayers({ tableId });
     return Responses._200({ message: "sendPlayers" });
   } catch (e) {
     console.log(e);
   }
 };
+
+async function updateSequence(tableId) {
+  const records = await Dynamo.scan(tableName, "tableId", tableId);
+  const writeToDB = records.Items.sort((a, b) =>
+    Number(a.sequenceNumber) > Number(b.sequenceNumber) ? 1 : -1
+  ).map((player, index) =>
+    Dynamo.write({ ...player, sequenceNumber: index + 1 }, tableName)
+  );
+  await Promise.all(writeToDB);
+}
