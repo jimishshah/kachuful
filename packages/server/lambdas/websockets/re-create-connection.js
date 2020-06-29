643ec80166e86262b40f46a00de3c32416739577
Jimish Shah <jimishshah@jimishs-mbp-2.home>
2020-06-30 00:14:57 +0100

    Remove unwanted code

--- packages/server/lambdas/websockets/re-create-connection.js
+++ packages/server/lambdas/websockets/re-create-connection.js
@@ -17,55 +17,45 @@ exports.handler = async (event) => {
       messageBody: { oldConnectionId },
     } = await getPlayerWithMessage(event);
 
-    let oldPlayerRow;
-    oldPlayerRow = await Dynamo.get(oldConnectionId, tableName);
-    // there is some kind of bug which i dont understand
-    // sometimes old id is not available but new id is available
-    // so if no results available for old id try new id
-    // this probably happens if second recreate connection is called with new id as old id
-    if (!oldPlayerRow.tableId) {
-      oldPlayerRow = await Dynamo.get(newConnectionId, tableName);
-    }
+    const oldPlayerRow = await Dynamo.get(oldConnectionId, tableName);
 
-    if (oldPlayerRow.tableId) {
-      const updatedPlayerRow = {
-        ...oldPlayerRow,
-        ID: newConnectionId,
-        oldConnectionId,
-        isDisconnected: false,
-      };
+    const updatedPlayerRow = {
+      ...oldPlayerRow,
+      ID: newConnectionId,
+      oldConnectionId,
+      isDisconnected: false,
+    };
 
-      await Promise.all([
-        Dynamo.delete(oldConnectionId, tableName),
-        Dynamo.write(updatedPlayerRow, tableName),
-      ]);
+    await Promise.all([
+      Dynamo.delete(oldConnectionId, tableName),
+      Dynamo.write(updatedPlayerRow, tableName),
+    ]);
 
-      const { tableId } = oldPlayerRow;
+    const { tableId } = oldPlayerRow;
 
-      logger({
-        message: "re- create-conneciton.js: 51",
-        debug_type: "RECREATE_CONNECTION_LOADDING",
-        newConnectionId,
-        event,
-        oldConnectionId,
-        oldPlayerRow,
-        updatedPlayerRow,
-        tableId,
-      });
-      const records = await Dynamo.query(
-        tableName,
-        indexName,
-        "tableId",
-        tableId
-      );
-      const players = records.Items.map((player) => player);
-      await WebSocket.send({
-        domainName,
-        stage,
-        connectionID: newConnectionId,
-        message: JSON.stringify({ players, action: "sendRecreateConnection" }),
-      });
-    }
+    logger({
+      message: "re- create-conneciton.js: 51",
+      debug_type: "RECREATE_CONNECTION_LOADDING",
+      newConnectionId,
+      event,
+      oldConnectionId,
+      oldPlayerRow,
+      updatedPlayerRow,
+      tableId,
+    });
+    const records = await Dynamo.query(
+      tableName,
+      indexName,
+      "tableId",
+      tableId
+    );
+    const players = records.Items.map((player) => player);
+    await WebSocket.send({
+      domainName,
+      stage,
+      connectionID: newConnectionId,
+      message: JSON.stringify({ players, action: "sendRecreateConnection" }),
+    });
     return Responses._200({ message: "connected" });
   } catch (e) {
     console.log(e);
